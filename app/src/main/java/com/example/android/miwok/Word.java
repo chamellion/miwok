3de9b4908ee043cf1067aea38418d69ad5d8fdf0
chamellion <37181865+chamellion@users.noreply.github.com>
2019-05-01 16:45:23 +0100

    Updated app to add audio to miwok translations

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,4 +1,56 @@
 package com.example.android.miwok;
 
+import android.widget.ImageView;
+
 public class Word {
+
+
+    private String mDefaultTranslation;
+
+    private String mMiwokTranslation ;
+
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    private int mAudio;
+
+
+
+//Constructor for word class with two input parameter, miwok word and corresponding English translation
+    public Word(String miwokTranslation, String DefaultTranslation, int Audio){
+        mDefaultTranslation = DefaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudio = Audio;
+    }
+    public Word(String miwokTranslation, String DefaultTranslation, int ImageResourceId, int Audio){
+        mDefaultTranslation = DefaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = ImageResourceId;
+        mAudio = Audio;
+    }
+    // get the default translation(English)
+    public String getDefaultTranslation(){
+        return mDefaultTranslation;
+    }// GEt the miwok translation
+    public String getMiwokTranslation(){
+        return mMiwokTranslation;
+    }public int getImageResourceId(){
+        return mImageResourceId;
+    }
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }public int getAudio(){
+        return mAudio;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mImageResourceId=" + mImageResourceId +
+                ", mAudio=" + mAudio +
+                '}';
+    }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,4 +1,58 @@
 package com.example.android.miwok;
 
-public class WordAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
+
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+       View listItemView = convertView;
+       if (listItemView == null){
+           listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent, false);
+       }
+       Word currentWord = getItem(position);
+
+        TextView nameTextview = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        nameTextview.setText(currentWord.getMiwokTranslation());
+
+        TextView nameTextView2 = (TextView) listItemView.findViewById(R.id.default_text_view);
+        nameTextView2.setText(currentWord.getDefaultTranslation());
+
+        ImageView imageView= (ImageView) listItemView.findViewById(R.id.image);
+
+        if (currentWord.hasImage()) {
+            imageView.setImageResource(currentWord.getImageResourceId());
+
+            imageView.setVisibility(View.VISIBLE);
+        }else {
+            imageView.setVisibility(View.GONE);
+        }
+        View textContainer = listItemView.findViewById(R.id.two_textViews);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
+
+            return listItemView;
+
+    }
 }
